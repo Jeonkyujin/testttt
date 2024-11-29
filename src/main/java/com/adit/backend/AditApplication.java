748785748337b0925774e1aee58c2c5128541367
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-11-30 03:34:38 +0900

    :tada: chore: Initial Commit

---
+++ src/main/java/com/adit/backend/AditApplication.java
@@ -0,0 +1,13 @@
+package com.adit.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adit/backend/AditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
