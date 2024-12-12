a3e673e33a2e9340a9cd5a2553404764877bcb3c
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-12-13 04:47:44 +0900

    :sparkles: feat: OAuth2 인증 시스템 구현 (#11) • 카카오 OAuth2 로그인 성공/실패 핸들러 구현 • OAuth2 사용자 정보 처리 및 변환 서비스 구현 • 카카오 프로필 정보 매핑 및 사용자 등록 처리

---
+++ src/main/java/com/adit/backend/global/security/oauth/CustomOAuth2UserService.java
@@ -0,0 +1,55 @@
+package com.adit.backend.global.security.oauth;
+
+import java.util.Map;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.adit.backend.domain.auth.dto.OAuth2UserInfo;
+import com.adit.backend.domain.auth.dto.model.PrincipalDetails;
+import com.adit.backend.domain.user.entity.User;
+import com.adit.backend.domain.user.repository.UserRepository;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+	private final UserRepository userRepository;
+
+	@Override
+	@Transactional
+	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+		// 유저 정보 가져오기
+		Map<String, Object> oAuth2UserAttributes = super.loadUser(userRequest).getAttributes();
+
+		// registrationId로 어떤 OAuth2 로그인인지 구분
+		String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+		String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+			.getUserInfoEndpoint().getUserNameAttributeName();
+
+		// OAuth2UserInfo 생성
+		OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfo.of(registrationId, oAuth2UserAttributes);
+
+		// 유저 생성 또는 업데이트
+		User user = getOrSaveUser(oAuth2UserInfo);
+
+		return new PrincipalDetails(user, oAuth2UserAttributes, userNameAttributeName);
+	}
+
+	private User getOrSaveUser(OAuth2UserInfo oAuth2UserInfo) {
+		User user = userRepository.findByEmail(oAuth2UserInfo.email())
+			.orElseGet(oAuth2UserInfo::toEntity);
+		user.decideSocialType();
+		return userRepository.save(user);
+	}
+}
+
+
