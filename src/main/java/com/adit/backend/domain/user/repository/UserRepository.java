0c2f6f2971abef018fc5d208264b113cc1df9d9d
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-12-13 04:37:15 +0900

    :sparkles: feat: 토큰 및 사용자 레포지토리 구현 (#11) - TokenRepository 인터페이스 구현 - UserRepository 인터페이스 구현

--- src/main/java/com/adit/backend/domain/user/repository/UserRepository.java
+++ src/main/java/com/adit/backend/domain/user/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package com.adit.backend.domain.user.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.adit.backend.domain.user.entity.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
+
+	Optional<User> findBySocialId(String socialId);
 }
