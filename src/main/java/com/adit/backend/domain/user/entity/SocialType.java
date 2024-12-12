6f04b6b9fa9181d06b13c7999b14353cb667b202
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-12-13 04:33:21 +0900

    sparkles: feat(entity): 사용자 및 토큰 도메인 구현 (#11) - User 엔티티 클래스 수정 - Token 엔티티 클래스 구현

---
+++ src/main/java/com/adit/backend/domain/user/entity/SocialType.java
@@ -0,0 +1,15 @@
+package com.adit.backend.domain.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public enum SocialType {
+	KAKAO("카카오"),
+	GOOGLE("구글"),
+	NAVER("네이버");
+
+	private final String description;
+}
--- src/main/java/com/adit/backend/domain/user/entity/User.java
+++ src/main/java/com/adit/backend/domain/user/entity/User.java
@@ -3,6 +3,7 @@ package com.adit.backend.domain.user.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.adit.backend.domain.auth.entity.Token;
 import com.adit.backend.domain.event.entity.Event;
 import com.adit.backend.domain.place.entity.UserPlace;
 import com.adit.backend.global.entity.BaseEntity;
@@ -10,62 +11,100 @@ import com.adit.backend.global.entity.BaseEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
 public class User extends BaseEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(nullable = false, unique = true)
-    private String email;
+	@Column(nullable = false, length = 10)
+	private String name;
 
-    @Column(nullable = false, length = 50)
-    private String nickname;
+	@Column(nullable = false, length = 20, unique = true)
+	private String email;
 
-    @Column(nullable = false, length = 20)
-    private String socialType;
+	@Column(nullable = false, length = 50)
+	private String nickname;
 
-    private String socialId;
-    private String token;
+	@Column(nullable = false)
+	private String profile;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Event> events = new ArrayList<>();
+	@Column(nullable = false, unique = true)
+	private String socialId;
 
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<UserPlace> userPlaces = new ArrayList<>();
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private SocialType socialType;
 
-    @OneToMany(mappedBy = "fromUser", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Friendship> sentFriendRequests = new ArrayList<>();
+	@Column(nullable = false)
+	@Enumerated(value = EnumType.STRING)
+	private Role role;
 
-    @OneToMany(mappedBy = "toUser", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Friendship> receivedFriendRequests = new ArrayList<>();
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private Token token;
 
-    // 연관관계 메서드
-    public void addEvent(Event event) {
-        this.events.add(event);
-        event.setUser(this);
-    }
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Event> events = new ArrayList<>();
 
-    public void addUserPlace(UserPlace userPlace) {
-        this.userPlaces.add(userPlace);
-        userPlace.setUser(this);
-    }
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<UserPlace> userPlaces = new ArrayList<>();
 
-    public void addFriendRequest(Friendship friendship) {
-        this.sentFriendRequests.add(friendship);
-        friendship.setFromUser(this);
-    }
+	@OneToMany(mappedBy = "fromUser", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Friendship> sentFriendRequests = new ArrayList<>();
+
+	@OneToMany(mappedBy = "toUser", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Friendship> receivedFriendRequests = new ArrayList<>();
+
+	@Builder
+	public User(String name, String email, String nickname, String profile, String socialId, SocialType socialType,
+		Role role) {
+		this.name = name;
+		this.email = email;
+		this.nickname = nickname;
+		this.profile = profile;
+		this.socialId = socialId;
+		this.socialType = socialType;
+		this.role = role;
+	}
+
+	// 연관관계 메서드
+	public void addEvent(Event event) {
+		this.events.add(event);
+		event.setUser(this);
+	}
+
+	public void addUserPlace(UserPlace userPlace) {
+		this.userPlaces.add(userPlace);
+		userPlace.setUser(this);
+	}
+
+	public void addFriendRequest(Friendship friendship) {
+		this.sentFriendRequests.add(friendship);
+		friendship.setFromUser(this);
+	}
+
+	public void changeNickName(String nickName) {
+		this.nickname = nickName;
+	}
+
+	public void decideSocialType() {
+		this.socialType = SocialType.KAKAO;
+	}
 }
