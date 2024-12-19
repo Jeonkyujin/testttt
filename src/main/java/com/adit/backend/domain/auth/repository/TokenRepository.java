0d2044e14b50d9699dbd41de03be416d105a9cd2
윤섭 <54024213+F-UMI@users.noreply.github.com>
2024-12-20 00:56:13 +0900

    :sparkles: feat: 토큰 Command 서비스 구현 및 적용 (#11)

--- src/main/java/com/adit/backend/domain/auth/repository/TokenRepository.java
+++ src/main/java/com/adit/backend/domain/auth/repository/TokenRepository.java
@@ -3,12 +3,13 @@ package com.adit.backend.domain.auth.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.adit.backend.domain.auth.entity.Token;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
-	void deleteByAccessToken(String accessToken);
 
 	Optional<Token> findTokenByRefreshToken(String refreshToken);
 
@@ -16,4 +17,8 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
 	Optional<Token> findByUserWithFetch(String socialId);
 
 	Optional<Token> findByAccessToken(String accessToken);
+
+	@Modifying
+	@Query("delete from Token t where t.accessToken = :accessToken")
+	void deleteByAccessToken(@Param("accessToken") String accessToken);
 }
---
+++ src/main/java/com/adit/backend/domain/auth/service/command/TokenCommandService.java
@@ -0,0 +1,54 @@
+package com.adit.backend.domain.auth.service.command;
+
+import static com.adit.backend.global.error.GlobalErrorCode.*;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.adit.backend.domain.auth.dto.response.KakaoResponse;
+import com.adit.backend.domain.auth.entity.Token;
+import com.adit.backend.domain.auth.repository.TokenRepository;
+import com.adit.backend.global.error.exception.TokenException;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class TokenCommandService {
+
+	private final TokenRepository tokenRepository;
+
+	public Token saveOrUpdateToken(KakaoResponse response) {
+		Token token = validateAccessToken(response);
+		log.info("Saving token: {}", token);
+		return tokenRepository.save(token);
+	}
+
+	public Token updateTokenEntity(KakaoResponse.AccessTokenDto response, String refreshToken) {
+		Token token = validateRefreshToken(refreshToken);
+		token.updateAccessToken(response.accessToken(), response.expiresIn());
+		log.info("Updating token: {}", token);
+		return tokenRepository.save(token);
+	}
+
+	public void deleteToken(String tokenValue) {
+		tokenRepository.deleteByAccessToken(tokenValue);
+		tokenRepository.flush();
+	}
+
+
+	private Token validateAccessToken(KakaoResponse response) {
+		return tokenRepository.findByAccessToken(response.accessToken())
+			.orElseGet(response::toEntity);
+
+	}
+
+	private Token validateRefreshToken(String refreshToken) {
+		return tokenRepository.findTokenByRefreshToken(refreshToken)
+			.orElseThrow(() -> new TokenException(TOKEN_NOT_FOUND));
+	}
+}
